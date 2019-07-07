6d9e8a72e0ce7b809caa0befcf9ffaef731702d3
Enrico <enrico.bison@spindoxlabs.it>
2019-07-07 16:22:04 +0200

    fix max and min

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import texttograph
 
-texttograph.plotfiles(['./testfiles/test_zero.txt', './testfiles/test_one.txt'])
+#texttograph.plotfiles(['./testfiles/test_zero.txt', './testfiles/test_one.txt'])
+texttograph.plotfiles(['./testfiles/test_single.txt'])
 #readers, points, graphs = plotfiles(['test_zucchero.txt'])
--- texttograph.py
+++ texttograph.py
@@ -5,6 +5,7 @@ import numpy as np
 
 readers = []
 points = []
+deltaminmax = 10
 
 def openfile(filename):
     textread = open(filename, "r")
@@ -13,20 +14,20 @@ def openfile(filename):
         reader.append(round(float(row), 3))
     return reader
 
-def one(files):
+def one(reader, files):
     plt.plot(points[0],readers[0],'ro')
-    plt.axis([0, len(readers[0]), -10, 10])
+    plt.axis([0, len(readers[0]), np.min(readers[0])-deltaminmax, np.max(readers[0])+deltaminmax])
     plt.title(os.path.basename(files[0]))
 
-def two(files):
+def two(readers, files):
     plt.subplot(121)
     plt.plot(points[0],readers[0],'ro')
-    plt.axis([0, len(readers[0]), -0.1, 0.1])
+    plt.axis([0, len(readers[0]), np.min(readers[0])-deltaminmax, np.max(readers[0])+deltaminmax])
     plt.title(os.path.basename(files[0]))
 
     plt.subplot(122)
     plt.plot(points[1],readers[1],'ro')
-    plt.axis([0, len(readers[1]), -0.2, 0.2])
+    plt.axis([0, len(readers[1]), np.min(readers[1])-deltaminmax, np.max(readers[1])+deltaminmax])
     plt.title(os.path.basename(files[1]))
 
 def three():
@@ -46,7 +47,7 @@ def plotfiles(files):
         points.append(p)
     
     plt.figure(1)
-    options[len(files)](files)
+    options[len(files)](readers, files)
     plt.show()
 
 
