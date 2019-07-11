16693779f722bab1690e5b5ad20dded589feab89
Enrico <enrico.bison@spindoxlabs.it>
2019-07-11 12:34:41 +0200

    bars overlapped

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ import texttograph
 
 #texttograph.plotfiles(['./testfiles/test_zero.txt', './testfiles/test_one.txt'], 'bar')
 #texttograph.plotfiles(['./testfiles/test_zero.txt', './testfiles/test_one.txt'], overlapped=True)
-texttograph.plotfiles(['./testfiles/tfile0.txt', './testfiles/tfile6.txt'], typeg="bar", overlapped=True)
+#texttograph.plotfiles(['./testfiles/tfile0.txt', './testfiles/tfile6.txt'], typeg="bar", overlapped=True)
+texttograph.plotfiles(['./testfiles/bars1.txt', './testfiles/bars2.txt'], typeg="bar", overlapped=True)
